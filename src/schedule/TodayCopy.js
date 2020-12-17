e45fbe49211465ee8d0c47688bf62dbc2312b7d3
M. Brock Podgurski <mbpod10@gmail.com>
2020-12-17 11:01:13 -0500

    got styling done

--- src/schedule/TodayCopy.js
+++ src/schedule/TodayCopy.js
@@ -1,43 +1,63 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./styles.css"
 import moment from "moment"
 import * as RBS from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import axios from "axios"
 
 
 const TodayCopy = () => {
 
-  // let value = moment()
-
   const [value, setValue] = useState(moment())
+  const [selectedMonth, setSelectedMonth] = useState("")
 
   const startDay = value.clone().startOf("week").subtract(1, "day")
   const endDay = value.clone().endOf("week")
 
+  const startMonth = moment().clone().startOf("month")
+  const endMonth = value.clone().add(12, "month")
+
   const startTime = value.clone().startOf("day").add(5, "hour")
   const endTime = value.clone().startOf("day").add(22, "hour")
 
-  // console.log(startDay.format("dddd DD-MM-YYYY"))
-  // console.log(endDay.format("dddd DD-MM-YYYY"))
-
-  // console.log(startTime.format("h:mm A"))
-  // console.log(endTime.format("h:mm A"))
-
   let weekArray = []
   let timeArray = []
+  let monthArray = []
+
+  // let weekArrayAPI = []
 
   while (startDay.isBefore(endDay, "day")) {
     weekArray.push(startDay.add(1, "day").clone())
+    // weekArrayAPI.push(startDay.add(1, "day").format(")
   }
 
   while (startTime.isBefore(endTime, "hour")) {
     timeArray.push(startTime.add(1, "hour").clone())
   }
 
+  while (startMonth.isBefore(endMonth, "month")) {
+    monthArray.push(startMonth.add(1, "month").clone())
+  }
+
+  const disMonthArray = monthArray.map((element, index) => {
+    return (
+      <>
+        <option key={index}>
+          {element.format("MMMM YYYY")}
+        </option>
+      </>
+    )
+  })
+
+  // console.log(weekArrayAPI)
+
   const disWeekArray = weekArray.map((element, index) => {
     return (
       <>
-        <th key={index}><Link to={`/detail/${element.format("YYYY-MM-DD")}`}>{element.format("ddd MM-DD-YYYY")}</Link></th>
+        <th className="table-head" key={index}><Link to={`/detail/${element.format("YYYY-MM-DD")}`}>{element.format("ddd MM-DD-YYYY")}</Link></th>
       </>
     )
   })
@@ -46,7 +66,7 @@ const TodayCopy = () => {
   const disTimeArray = timeArray.map((element, index) => {
     return (
       <tr key={index}>
-        <th>{element.format("h:mm A")}</th>
+        <th className="table-head">{element.format("h:mm A")}</th>
         <th></th>
         <th></th>
         <th></th>
@@ -58,33 +78,81 @@ const TodayCopy = () => {
     )
   })
 
-  console.log(value.clone().startOf("week").add(7, "day").format("dddd MM/DD/YYYY"))
+  // console.log(value.clone().startOf("week").add(7, "day").format("dddd MM/DD/YYYY"))
 
   const nextWeek = () => {
-    // value = value.clone().startOf("week").add(7, "day")
     setValue(value.clone().startOf("week").add(7, "day"))
-    console.log(value.format("dddd MM/DD/YYYY"))
   }
   const previousWeek = () => {
-    // value = value.clone().startOf("week").add(7, "day")
     setValue(value.clone().startOf("week").subtract(7, "day"))
-    console.log(value.format("dddd MM/DD/YYYY"))
   }
+
+  const changeMonth = (event) => {
+    if (event.target.value === "Today") {
+      setValue(moment())
+    } else {
+      setValue(moment(event.target.value))
+    }
+  }
+
+  useEffect(() => {
+    const makeAPICall = () => {
+      weekArray.forEach((element, index) => {
+        axios.get(`http://127.0.0.1:8000/days/${element.format("YYYY-MM-DD")}/appointments/`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              // 'Authorization': 'Token 43e647722ff3daa8561ab96a60d57298fe23d45e'
+            }
+          })
+          .then((response) => {
+            // console.log(response.data)
+            if (response.data.day) {
+              console.log(response.data.day)
+            }
+            else if (response.data.no_day) {
+              console.log(response.data)
+            }
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+    }
+    makeAPICall()
+  })
+
+
+
 
   return (
     <>
-      <h1>{value.clone().startOf("week").format("MMMM YYYY")}</h1>
-      <div class="header">
-        <h5 className="weekly-header">{value.clone().startOf("week").format("ddd MM/DD/YY")} - {endDay.format("ddd MM/DD/YY")}</h5>
-        <div onClick={() => previousWeek()}>Previous</div>
-        <div onClick={() => nextWeek()}>Next</div>
+      <div className="month-div">
+        <h1 className="month" >{value.clone().startOf("week").format("MMMM YYYY")}</h1>
+      </div>
 
+      <div className="header">
+        <div onClick={() => previousWeek()}><FontAwesomeIcon icon={faArrowAltCircleLeft} /></div>
+        <h5 className="weekly-header">{value.clone().startOf("week").format("ddd MM/DD/YY")} - {endDay.format("ddd MM/DD/YY")}</h5>
+        <div onClick={() => nextWeek()}><FontAwesomeIcon icon={faArrowAltCircleRight} /></div>
+      </div>
+
+      <div className="form-div">
+        <RBS.Form>
+          <RBS.Form.Group as={RBS.Col} controlId="formGridState">
+            <RBS.Form.Control as="select" defaultValue="Month..." onChange={changeMonth} value={selectedMonth}>
+              <option>Month...</option>
+              <option>Today</option>
+              {disMonthArray}
+            </RBS.Form.Control>
+          </RBS.Form.Group>
+        </RBS.Form>
       </div>
 
       <RBS.Table striped bordered hover variant="sm">
         <thead>
           <tr>
-            <th>Date</th>
+            <th className="table-head">Date</th>
             {disWeekArray}
           </tr>
         </thead>
